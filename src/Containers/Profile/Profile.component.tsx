cf242dafcf25e188c41656df4b823ea5c8133c1a
Ganz <gandarainpanjaitan@Ganzs-Mac-mini.local>
2025-10-29 21:27:04 +0700

    enhance profile screen

--- src/Containers/Profile/Profile.component.tsx
+++ src/Containers/Profile/Profile.component.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { ScrollView, Text, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native'
+import {
+  View,
+  ScrollView,
+  Text,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
+import MaterialIcons from '@react-native-vector-icons/material-design-icons'
 
 import HeaderBackgroundSecond from '../../Assets/Images/HeaderBackgroundSecond.png'
+import Colors from '../../Assets/Colors'
+import { scaleFont } from '../../Utils/Size'
 
 import styles from './Profile.styles'
 
@@ -15,6 +25,38 @@ const renderImageBackground = () => (
   </ImageBackground>
 )
 
+const renderAccountContainer = () => (
+  <View style={styles.accountContainer}>
+    <Text style={styles.textTitleContent}>Akun</Text>
+  </View>
+)
+
+const renderEmailContainer = () => (
+  <View style={styles.emailContainer}>
+    <View style={styles.emailContent}>
+      <MaterialIcons color={Colors.BLACK} name="at" size={scaleFont(12)} />
+      <Text style={styles.textTitleSubContent}>Email</Text>
+    </View>
+    <Text style={styles.textDescriptionContent}>gandarainpanjaitan@gmail.com</Text>
+  </View>
+)
+
+const renderFullNameContainer = () => (
+  <View style={styles.emailContainer}>
+    <View style={styles.emailContent}>
+      <MaterialIcons color={Colors.BLACK} name="book-account" size={scaleFont(12)} />
+      <Text style={styles.textTitleSubContent}>Nama Lengkap</Text>
+    </View>
+    <Text style={styles.textDescriptionContent}>Ganda Rain Panjaitan</Text>
+  </View>
+)
+
+const renderSettingContainer = () => (
+  <View style={styles.accountContainer}>
+    <Text style={styles.textTitleContent}>Pengaturan</Text>
+  </View>
+)
+
 const Profile = (): React.JSX.Element => (
   <KeyboardAvoidingView
     behavior={Platform.OS === 'ios' ? 'padding' : undefined}
@@ -22,6 +64,12 @@ const Profile = (): React.JSX.Element => (
   >
     <ScrollView contentContainerStyle={styles.contentContainerStyle}>
       {renderImageBackground()}
+      {renderAccountContainer()}
+      {renderEmailContainer()}
+      {renderFullNameContainer()}
+      {renderSettingContainer()}
+      {renderEmailContainer()}
+      {renderFullNameContainer()}
     </ScrollView>
   </KeyboardAvoidingView>
 )
